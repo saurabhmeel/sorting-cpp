1673e26dc6bf7b58f1fd4ab6eed98fc1b7effc58
saurabhmeel <88885107+saurabhmeel@users.noreply.github.com>
2022-10-14 09:17:37 +0530

    Add files via upload

---
+++ sortarr.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void solve( int arr[], int n)
+{
+   sort(arr,arr+n);
+   for(int i =0 ; i<n;i++)
+     cout<<arr[i]<<" ";
+}
+
+int main()
+{
+    int arr[] = {5 , 6 ,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    solve( arr,n);
+    return 0;
+}
